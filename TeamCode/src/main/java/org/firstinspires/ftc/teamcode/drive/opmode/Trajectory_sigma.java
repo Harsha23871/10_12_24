40ea2f833b10f7d340d0336daaa3d28532bcf3ac
Harsha Roadrunner Pre-Localiaztion 9/21/24 <lostharsha23871@gmail.com>
2024-10-20 12:37:45 -0500

    The newest best project works to spline and the trajectory works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Trajectory_sigma.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Trajectory_sigma.java
@@ -3,27 +3,61 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
+@Autonomous(group = "a", name="Test RR auto")
 public class Trajectory_sigma extends LinearOpMode {
 
     @Override
+
+// 12 -58
+
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startPose = new Pose2d(-58, 12, Math.toRadians(0));
+        drive.setPoseEstimate(startPose);
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                .strafeRight(10)
-                .forward(5)
+
+
+        Trajectory Traj1 = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(26,41),0)
                 .build();
 
+
+        Trajectory myTrajectory = drive.trajectoryBuilder(Traj1.end())
+                .splineTo(new Vector2d(52, 82), 0)
+                .build();
         waitForStart();
 
         if(isStopRequested()) return;
 
+        drive.followTrajectory(Traj1);
         drive.followTrajectory(myTrajectory);
+//splineTo(new Vector2d(x1, y1), heading)
+         //       .splineTo(new Vector2d(x2, y2), heading)
+         //       .build();
+
+// -12 -48
+
+       /* SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(12, -62,Math.toRadians(270));
+        TrajectorySequence middleSpike = drive.trajectorySequenceBuilder(startPose)
+                .lineTo(new Vector2d(12,-62))
+//                .turn(Math.toRadians(45)) // Turns 45 degrees counter-clockwise
+                .build();
+
+
+        waitForStart();
+        drive.followTrajectorySequence(middleSpike);//might need to change*/
+
+
     }
 
 
