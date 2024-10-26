90cc53ced46d909c776f49b5dcd76a08fec850c4
Harsha Roadrunner Pre-Localiaztion 9/21/24 <lostharsha23871@gmail.com>
2024-10-26 16:56:25 -0500

    The newest best project works to spline and the trajectory works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -30,8 +30,6 @@ import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationCon
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.qualcomm.hardware.lynx.LynxModule;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -39,11 +37,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoImpl;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Trajectory_sigma.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Trajectory_sigma.java
@@ -2,12 +2,16 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
@@ -19,26 +23,39 @@ public class Trajectory_sigma extends LinearOpMode {
 // 12 -58
 
     public void runOpMode() {
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        Pose2d startPose = new Pose2d(-58, 12, Math.toRadians(0));
+        Pose2d startPose = new Pose2d(-36, -58, Math.toRadians(90)); // =36, -59
         drive.setPoseEstimate(startPose);
 
 
-
-        Trajectory Traj1 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(26,41),0)
+        TrajectorySequence trajectory7 = drive.trajectorySequenceBuilder(new Pose2d(-36.41, -59.99, Math.toRadians(90.00)))
+                .splineTo(new Vector2d(-40.86, -45.60), Math.toRadians(107.19))
+                .splineTo(new Vector2d(-44.42, -36.11), Math.toRadians(110.56))
+                .splineTo(new Vector2d(-49.31, -25.73), Math.toRadians(115.24))
+                .splineTo(new Vector2d(-51.68, -30.33), Math.toRadians(242.70))
+                .splineTo(new Vector2d(-55.84, -38.48), Math.toRadians(243.02))
+                .splineTo(new Vector2d(-59.84, -49.16), Math.toRadians(249.44))
+                .splineTo(new Vector2d(-61.92, -60.73), Math.toRadians(259.82))
                 .build();
 
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(Traj1.end())
-                .splineTo(new Vector2d(52, 82), 0)
-                .build();
+     //   Trajectory myTrajectory = drive.trajectoryBuilder(Traj1.end())
+     //           .splineTo(new Vector2d(48 , -48), 0)
+      //          .build();
         waitForStart();
 
         if(isStopRequested()) return;
 
-        drive.followTrajectory(Traj1);
-        drive.followTrajectory(myTrajectory);
+        drive.followTrajectorySequence(trajectory7);
+
+      /*  Pose2d poseEstimate = drive.getPoseEstimate();
+        telemetry.addData("x", poseEstimate.getX());
+        telemetry.addData("y", poseEstimate.getY());
+        telemetry.addData("heading", poseEstimate.getHeading());
+        telemetry.update();*/
+
+     //   drive.followTrajectory(myTrajectory);
 //splineTo(new Vector2d(x1, y1), heading)
          //       .splineTo(new Vector2d(x2, y2), heading)
          //       .build();
